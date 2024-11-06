5453bebd7b86cb996b7772ab3f7a490a9cad6f74
elham <elhamazizi05@gmail.com>
2024-11-06 16:46:35 +0000

    ticket 6

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom"
 
 function Navigation() {
-    return (
-        <nav>
-          <ul className='nav-ul'>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/articles">Articles</Link>
-            </li>
-          </ul>
-        </nav>
-    )
+  return (
+      <nav>
+        <ul className='nav-ul'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+        </ul>
+      </nav>
+  )
 }
 
 export default Navigation
